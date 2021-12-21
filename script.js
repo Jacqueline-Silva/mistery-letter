f84e52272f5441cb63a49e910ab46c93d9bd1fec
Jacqueline Silva <jacque.sx@hotmail.com>
2021-12-21 00:02:46 -0300

    iniciando o projeto x

---
+++ script.js
@@ -0,0 +1,24 @@
+let inpCartaTexto = document.getElementById('carta-texto');
+let pCartaGerada = document.getElementById('carta-gerada');
+let btnCriarCarta = document.getElementById('criar-carta');
+
+function geraCarta() {
+  pCartaGerada.removeChild(pCartaGerada.firstChild);
+
+  let valueInput = inpCartaTexto.value.split(' ');
+  if (valueInput !== '') {
+    for (let palavra in valueInput) {
+      let spanDoP = document.createElement('span');
+      spanDoP.innerHTML = valueInput[palavra];
+      pCartaGerada.appendChild(spanDoP);
+    }
+  }
+}
+btnCriarCarta.addEventListener('click', geraCarta);
+
+function alertaCarta() {
+  if (inpCartaTexto.value === '') {
+    pCartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  }
+}
+btnCriarCarta.addEventListener('click', alertaCarta);
