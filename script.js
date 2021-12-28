4a3024150628e22a93e78102400e2be0b75a2023
Jacqueline Silva <jacque.sx@hotmail.com>
2021-12-28 13:13:48 -0300

    Requisitos 3 e 18

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let inpCartaTexto = document.getElementById('carta-texto');
-let pCartaGerada = document.getElementById('carta-gerada');
-let btnCriarCarta = document.getElementById('criar-carta');
+const inpCartaTexto = document.getElementById('carta-texto');
+const pCartaGerada = document.getElementById('carta-gerada');
+const btnCriarCarta = document.getElementById('criar-carta');
 
 function geraCarta() {
-  let valueInput = inpCartaTexto.value.split(' ');
+  const valueInput = inpCartaTexto.value.split(' ');
+  while (pCartaGerada.firstChild) {
+    pCartaGerada.removeChild(pCartaGerada.firstChild);
+  }
+  /*
+REF: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
+     A seleção dos filhos é feita pelo elemento pai.
+(while (condição true) {se 'true' execute 'x'} é parecido com if/else para condição e for para repetição)
+REF: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while
+  */
   if (valueInput !== '') {
-    for (let palavra in valueInput) {
-      let spanDoP = document.createElement('span');
+    for (let palavra = 0; palavra < valueInput.length; palavra += 1) {
+      const spanDoP = document.createElement('span');
       spanDoP.innerHTML = valueInput[palavra];
       pCartaGerada.appendChild(spanDoP);
     }
@@ -20,3 +29,14 @@ function alertaCarta() {
   }
 }
 btnCriarCarta.addEventListener('click', alertaCarta);
+/*
+const iptReplace = inpCartaTexto.value.replace(/' '/, '');
+ || inpCartaTexto.value === iptReplace
+*/
+
+function conta() {
+  const pContador = document.getElementById('carta-contador');
+  const contaFilhos = pCartaGerada.childNodes.length;
+  pContador.innerHTML = `${contaFilhos}`;
+}
+btnCriarCarta.addEventListener('click', conta);
