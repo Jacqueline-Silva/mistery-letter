ad41dc0cfcb0cb8c38eccf5820f3e056319ddfb8
Jacqueline Silva <jacque.sx@hotmail.com>
2021-12-28 14:45:17 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -4,34 +4,30 @@ const btnCriarCarta = document.getElementById('criar-carta');
 
 function geraCarta() {
   const valueInput = inpCartaTexto.value.split(' ');
+
   while (pCartaGerada.firstChild) {
     pCartaGerada.removeChild(pCartaGerada.firstChild);
   }
-  /*
-REF: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
-     A seleção dos filhos é feita pelo elemento pai.
-(while (condição true) {se 'true' execute 'x'} é parecido com if/else para condição e for para repetição)
-REF: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while
-  */
-  if (valueInput !== '') {
-    for (let palavra = 0; palavra < valueInput.length; palavra += 1) {
-      const spanDoP = document.createElement('span');
-      spanDoP.innerHTML = valueInput[palavra];
-      pCartaGerada.appendChild(spanDoP);
-    }
+
+  if (inpCartaTexto.value === '' || !inpCartaTexto.value.trim()) {
+    pCartaGerada.innerHTML = '<em>Por favor, digite o conteúdo da carta.</em>';
+  }
+
+  for (let palavra = 0; palavra < valueInput.length; palavra += 1) {
+    const spanDoP = document.createElement('span');
+    spanDoP.innerHTML = valueInput[palavra];
+    pCartaGerada.appendChild(spanDoP);
   }
 }
 btnCriarCarta.addEventListener('click', geraCarta);
 
-function alertaCarta() {
-  if (inpCartaTexto.value === '') {
-    pCartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
-  }
-}
-btnCriarCarta.addEventListener('click', alertaCarta);
 /*
-const iptReplace = inpCartaTexto.value.replace(/' '/, '');
- || inpCartaTexto.value === iptReplace
+REF: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
+
+REF: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while > A seleção dos filhos é feita pelo elemento pai.
+(while (condição true) {se 'true' execute 'x'} é parecido com if/else para condição e for para repetição).
+
+REF: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim  > usado para remover espaços em branco.
 */
 
 function conta() {
